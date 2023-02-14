a813d7013eca35daba446b34838316c147dfbf72
Ed <polet.edouard@gmail.com>
2023-02-14 16:31:49 +0100

    work in progress HP

--- src/components/searchbar.jsx
+++ src/components/searchbar.jsx
@@ -5,7 +5,7 @@ import { PlusIcon } from '@heroicons/react/24/outline'
 
 const SearchBar = () =>{
     return (
-        <form className="flex justify-center" >
+        <form className="flex">
             <input type='text' id="ingredient-search" placeholder="Type your ingredients ..." className="bg-gray-300 rounded-l-md h-8 w-96 pl-2 outline-0 placeholder-gray-400 placeholder
             "></input>
             <button type="submit" className="rounded-r-md bg-gray-300 h-8 pr-2">< PlusIcon className="text-gray-700 h-6 w-6"/>
