d74a8248ef5d65330a162a070333239c1d3f13b3
egidehirwa <egidehirwam@gmail.com>
2023-02-14 16:48:57 +0100

    Setup of single page for recipies

--- next.config.js
+++ next.config.js
@@ -3,6 +3,16 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-}
+  /*   images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.ibb.co",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  }, */
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,7 +1,10 @@
+import { PlusIcon } from "@heroicons/react/24/solid";
+
 export default function About() {
   return (
     <main>
       <div>This is the About page app</div>
+      <PlusIcon className="h-6 w-6 text-blue-500" />
     </main>
   );
 }
