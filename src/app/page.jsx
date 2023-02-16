96fb39d69663828496557eacc80b931a6dc6f3d9
Kawtar <kawtarzarioh@gmail.com>
2023-02-16 15:04:34 +0100

    update picture

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,3 @@
-
-
 import SearchBar from "@/components/searchbar";
 import { Rubik, Inter } from "@next/font/google";
 
@@ -21,19 +19,23 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <main className="justify-center flex flex-col ">
-      <div className=" w-full flex  bg-orange-600 "><h1>Header</h1></div>
+      <div className=" w-full flex  bg-orange-600 ">
+        <h1>Header</h1>
+      </div>
       <div className=" text-center bg-orange-100">
         <div>
           <h1>What is in your fridge</h1>
         </div>
         <div className="flex justify-center">
-          <SearchBar/>
-          <button className="ml-2 pl-2 pr-2 rounded-md bg-red-700 text-white">Search for recipe</button>
-
+          <SearchBar />
+          <button className="ml-2 pl-2 pr-2 rounded-md bg-red-700 text-white">
+            Search for recipe
+          </button>
         </div>
       </div>
-      <div className=" flex self-center w-full bg-orange-200"><h1>Footer</h1></div>
+      <div className=" flex self-center w-full bg-orange-200">
+        <h1>Footer</h1>
+      </div>
     </main>
-    
   );
 }
