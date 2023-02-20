9993160f1dde7c6ba9450fa922b599a9c430f3b0
Polet Edouard <113932560+Ed2207@users.noreply.github.com>
2023-02-20 16:30:04 +0100

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -28,7 +28,7 @@ export default function Home() {
     <main className="justify-center flex flex-col h-screen ">
       <div className=" text-center bg-red-100 h-3/5 flex flex-col ">
         <div className="pb-8">
-          <h1 className=" atext-7xl">What is in your fridge ?</h1>
+          <h1 className=" text-7xl">What is in your fridge ?</h1>
         </div>
         <div className="flex justify-center ">
           <SearchBar/>
