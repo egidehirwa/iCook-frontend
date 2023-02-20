2d43ae1f951f0ae07ccb8720c9b5b47a9628987f
Ed <polet.edouard@gmail.com>
2023-02-20 16:25:03 +0100

    HP done

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 const Footer = () => {
   return (
     <div className="bg-grey h-[80px] text-[15px] uppercase">
-      <div className='container mx-auto flex flex-row items-center'>
+      <div className='container mx-auto flex flex-row items-center pt-5'>
         <div className='w-1/2 flex'>
           <p>©2023 - ICook. All rights reserved.</p>
         </div>
