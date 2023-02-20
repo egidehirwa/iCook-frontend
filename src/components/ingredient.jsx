bc3b2f9b4c1538224d4d0dbcc10d357c759ea02a
Ed <polet.edouard@gmail.com>
2023-02-20 13:54:42 +0100

    commit homepage (not done yet)

---
+++ src/components/ingredient.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Ingredient = ()=>{
+ return(
+    <div name='ingredients container'className=" pt-5 flex justify-center">
+        <div className="ml-5 p-2 border border-red-700 bg-white rounded shadow-outline">
+            <h3>Picture</h3>
+            <h3>name</h3>
+        </div>
+       
+    </div>
+ )
+}
+
+export default Ingredient;
