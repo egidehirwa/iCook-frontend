6e6ff612d4cf387fdaa014c3187b99813809019c
Kawtar <kawtarzarioh@gmail.com>
2023-02-20 14:36:00 +0100

    nn

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import "./globals.css";
 
 export default function RootLayout({ children }) {
@@ -11,19 +10,5 @@ export default function RootLayout({ children }) {
       <head />
       <body>{children}</body>
     </html>
-=======
-import React from 'react';
-import Header from "../components/header";
-import Footer from "../components/footer";
-import './globals.css';
-
-export default function RootLayout({ children }) {
-  return (
-    <div className='flex flex-col min-h-screen'>
-      <Header />
-      <main className='flex-grow'>{children}</main>
-      <Footer />
-    </div>
->>>>>>> 2b5d604a430f1cc4b1685cd0bce086f4d4f50501
   );
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -24,7 +24,6 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-<<<<<<< HEAD
     <main className="justify-center flex flex-col ">
       <div className=" w-full flex  bg-orange-600 ">
         <h1>Header</h1>
@@ -38,18 +37,6 @@ export default function Home() {
           <button className="ml-2 pl-2 pr-2 rounded-md bg-red-700 text-white">
             Search for recipe
           </button>
-=======
-    <main className="justify-center flex flex-col h-screen ">
-      <div className=" w-full flex  bg-orange-600 "><h1>Header</h1></div>
-      <div className=" text-center bg-orange-100 h-4/5 flex flex-col justify-center">
-        <div className="pb-8">
-          <h1 className="text-5xl">What is in your fridge ?</h1>
-        </div>
-        <div className="flex justify-center ">
-          <SearchBar/>
-          <button className="ml-2 pl-2 pr-2 rounded-md bg-red-700 border-2 border-red-700 text-white transition-colors duration-700 transform hover:bg-white hover:text-red-700 hover:border-2 hover:border-red-700">Search for recipe</button>
-
->>>>>>> 2b5d604a430f1cc4b1685cd0bce086f4d4f50501
         </div>
         <Ingredient />
       </div>
