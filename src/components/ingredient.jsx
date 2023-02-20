b57dde1e3199eb48ae0d38205010261e00497918
Ed <polet.edouard@gmail.com>
2023-02-20 16:59:36 +0100

    add a x in ingredientCard

--- src/components/ingredient.jsx
+++ src/components/ingredient.jsx
@@ -4,7 +4,8 @@ import logo from "/src/public/icook-logo.png"
 const Ingredient = ()=>{
  return(
     <div name='ingredients container'className=" pt-5 flex justify-center">
-       <div name= 'ingredientCard' className="flex flex-col h-28 w-28 bg-white border rounded-xl ml-5 items-center">
+       <div name= 'ingredientCard' className="flex flex-col h-32 w-32 bg-white border rounded-xl ml-5 items-center">
+        <button className="text-grey ml-24 ">x</button>
        <Image src={logo} alt="random pic" className="h-24 w-24"/>
        <h3 className="p-1 bg-yellow border rounded-3xl pl-8 pr-8">name</h3>
        </div>
