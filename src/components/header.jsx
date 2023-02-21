5a16ac2e7ae4c285f8bdaa8dcc398d9c1b225eaa
Daria <spodina.darya@gmail.com>
2023-02-21 16:55:27 +0100

    responsive header, footer and search result page

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -4,22 +4,22 @@ import logo from "/src/public/icook-logo.png";
 
 const Header = () => {
   return (
-    <div className="container mx-auto h-[150px] text-[18px] lg:flex justify-between items-center">
+    <div className="container lg:mx-auto lg:h-[150px] text-[18px] flex lg:flex-row lg:justify-between lg:items-center">
       <Link href="/">
         <Image
           src={logo}
           alt="iCook Logo"
-          className="w-[220px] pt-12 md:items-center"
+          className="w-[220px] pt-12"
         />
       </Link>
-      <div className="flex justify-around lg:w-1/2 pt-6 border-gray-600">
-        <button className="bg-yellow px-8 min-h-[40px] items-center rounded-xl hover:bg-white hover:border-[1px] hover:border-gray-600">
-          <Link href="/recipe-by-category" className="">
+      <div className="flex flex-col lg:flex-row justify-around lg:w-1/2 pt-6 border-gray-600">
+        <button className="bg-yellow shadow-md px-8 min-h-[40px] items-center rounded-xl hover:bg-white hover:border-[1px] hover:border-gray-600">
+          <Link href="/search-result" className="">
             Recipe by category
           </Link>
         </button>
-        <button className="bg-yellow px-8 items-center rounded-xl hover:bg-white hover:border-[1px] hover:border-gray-600">
-          <Link href="/RandomRecipe" className="">
+        <button className="bg-yellow shadow-md px-8 min-h-[40px] items-center rounded-xl hover:bg-white hover:border-[1px] hover:border-gray-600">
+          <Link href="/randomRecipe" className="">
             Random Recipe
           </Link>
         </button>
