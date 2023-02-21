b6be98f7a8c55e2a2a991c329a86762e8f2e0fef
egidehirwa <egidehirwam@gmail.com>
2023-02-21 11:04:20 +0100

    merge with main

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,8 @@
-
-
 import SearchBar from "@/components/searchbar";
 import { Rubik, Inter } from "@next/font/google";
+import Ingredient from "@/components/ingredient";
+
+
 
 // Testing font variables with Tailwind
 
@@ -18,22 +19,23 @@ const poppins = Poppins({
 const rubik = Rubik({ subsets: ["latin"] });
 const inter = Inter({ subsets: ["latin"] });
 
+
+
+
 export default function Home() {
   return (
-    <main className="justify-center flex flex-col ">
-      <div className=" w-full flex  bg-orange-600 "><h1>Header</h1></div>
-      <div className=" text-center bg-orange-100">
-        <div>
-          <h1>What is in your fridge</h1>
-        </div>
-        <div className="flex justify-center">
-          <SearchBar/>
-          <button className="ml-2 pl-2 pr-2 rounded-md bg-red-700 text-white">Search for recipe</button>
 
+    <main className="justify-center flex flex-col h-screen ">
+      <div className=" text-center bg-red-100 h-3/5 flex flex-col ">
+        <div className="pb-8">
+          <h1 className="font-satisfy text-7xl">What is in your fridge ?</h1>
         </div>
+        <div className="flex justify-center ">
+          <SearchBar/>
+          <button className="ml-2 pl-2 pr-2 rounded-md h-12 bg-red border-2 border-red text-white transition-colors transform hover:bg-white hover:text-red hover:border-2 hover:border-red">Search for recipe</button>
+        </div>
+        <Ingredient />
       </div>
-      <div className=" flex self-center w-full bg-orange-200"><h1>Footer</h1></div>
     </main>
-    
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,11 +9,71 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    screens: {
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "1536px",
+      // => @media (min-width: 1536px) { ... }
+  },
     extend: {
+      colors: {
+        red: "#B04137",
+        lightgrey: "#F6F4F2",
+        grey: "#E7E2DE",
+        yellow: "#EED06C",
+    },
       fontFamily: {
         sans: ["var(--font-rubik)"],
         mono: ["var(--font-poppins)"],
-      },
+        satisfy: ['Satisfy', 'cursive'],
+      }, 
+      borderRadius: {
+        default: "4px",
+        large: "12px",
+    },
+    padding: {
+        sm: "8px",
+        md: "16px",
+        lg: "24px",
+        xl: "48px",
+    },
+    spacing: {
+        sm: "8px",
+        md: "16px",
+        lg: "24px",
+        xl: "48px",
+    },
+    maxWidth: {
+        1: "100%",
+        "3/4": "75%",
+        "1/2": "50%",
+        "1/4": "25%",
+        "1/5": "20%"
+    },
+    maxHeight: {
+        1: "100%",
+        "3/4": "75%",
+        "1/2": "50%",
+        "1/4": "25%",
+        "1/5": "20%"
+    },
+    minWidth: {
+        1: "100%",
+        "3/4": "75%",
+        "1/2": "50%",
+        "1/4": "25%",
+        "1/5": "20%"
+    },
     },
   },
   plugins: [],
