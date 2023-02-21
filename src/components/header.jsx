6e09edba92cc731a6fb55c40344fb6b9215dae00
Daria <spodina.darya@gmail.com>
2023-02-21 10:39:28 +0100

    upd before pull

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -9,11 +9,11 @@ const Header = () => {
         <Image
           src={logo}
           alt="iCook Logo"
-          className="w-[250px] pt-12 md:items-center"
+          className="w-[220px] pt-12 md:items-center"
         />
       </Link>
-      <div className="flex justify-around lg:w-1/2 border-b-[1px] pb-6 border-gray-600 space-x-1">
-        <button className="bg-yellow px-8 min-h-[50px] items-center rounded-xl hover:bg-white hover:border-[1px] hover:border-gray-600">
+      <div className="flex justify-around lg:w-1/2 pt-6 border-gray-600">
+        <button className="bg-yellow px-8 min-h-[40px] items-center rounded-xl hover:bg-white hover:border-[1px] hover:border-gray-600">
           <Link href="/recipe-by-category" className="">
             Recipe by category
           </Link>
