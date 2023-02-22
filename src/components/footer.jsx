36d2867a132bb0a4bc7fb071bcfb6c842e2dbe13
Daria <spodina.darya@gmail.com>
2023-02-22 10:35:21 +0100

    Fixed conflict in footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,13 +3,8 @@ import Link from "next/link";
 const Footer = () => {
   return (
     <div className="bg-grey h-[80px] text-[15px] uppercase">
-<<<<<<< HEAD
       <div className='container mx-auto flex flex-col lg:flex-row align-center pt-5'>
         <div className='lg:w-1/2 flex'>
-=======
-      <div className="container mx-auto flex flex-row items-center pt-5">
-        <div className="w-1/2 flex">
->>>>>>> 3e2ef6e73e3730f3b6ad839270a711ab7e0ddac3
           <p>©2023 - ICook. All rights reserved.</p>
         </div>
         <div className="lg:w-1/2 flex justify-around">
