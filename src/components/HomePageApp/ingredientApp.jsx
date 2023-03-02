90f232df07910e76f90b7ce1085574cecf20d9d3
egidehirwa <egidehirwam@gmail.com>
2023-03-02 14:27:26 +0100

    Added a div container to the ingredients section

--- src/components/HomePageApp/ingredientApp.jsx
+++ src/components/HomePageApp/ingredientApp.jsx
@@ -1,39 +1,42 @@
-'use client';
+"use client";
 
-import React, {useState} from "react";
+import React, { useState } from "react";
 import SearchBar from "./searchbar";
 import IngredientCard from "./ingredientCard";
 
 const IngredientApp = () => {
-    const [searchValue, setSearchValue] = useState('');
-    const [ingredients, setIngredients] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
+  const [ingredients, setIngredients] = useState([]);
 
-    const handleDeleteIngredient = (index) => {
-        setIngredients(ingredients.filter((_, i) => i !== index));
-      };
-    
+  const handleDeleteIngredient = (index) => {
+    setIngredients(ingredients.filter((_, i) => i !== index));
+  };
 
-    const handleSearchSubmit = (value) => {
-        setSearchValue("");
-        setIngredients([...ingredients, value]);
-      };
-   
+  const handleSearchSubmit = (value) => {
+    setSearchValue("");
+    setIngredients([...ingredients, value]);
+  };
 
-    return(
-        <div className="flex flex-col">
-           <div>
-           <SearchBar handleSearchSubmit={handleSearchSubmit}  searchValue={searchValue}
-          setSearchValue={setSearchValue}/>
-           </div>
-           <div className="flex justify-center">
-           {ingredients.map((ingredient, index) => (
-          <IngredientCard key={index} searchValue={ingredient} onDelete={() => handleDeleteIngredient(index)} />
+  return (
+    <div className="flex flex-col">
+      <div>
+        <SearchBar
+          handleSearchSubmit={handleSearchSubmit}
+          searchValue={searchValue}
+          setSearchValue={setSearchValue}
+        />
+      </div>
+      <div className="flex justify-center flex-wrap max-w-sm">
+        {ingredients.map((ingredient, index) => (
+          <IngredientCard
+            key={index}
+            searchValue={ingredient}
+            onDelete={() => handleDeleteIngredient(index)}
+          />
         ))}
-           </div>
-        </div>
+      </div>
+    </div>
+  );
+};
 
-    )
-}
-
-
-export default IngredientApp
+export default IngredientApp;
